3f0cbabe13a2688ce22181e4d3e38f5fffea8194
mohammed <71414472+mohammedhrima@users.noreply.github.com>
2022-12-24 23:22:15 +0100

    Update 007.c

--- to_learn/004/007.c
+++ to_learn/004/007.c
@@ -8,6 +8,7 @@
 #define green 0x0000FF00
 #define blue 0x000000FF
 #define len 10
+#define points_len 5
 
 typedef struct s_var
 {
@@ -15,9 +16,9 @@ typedef struct s_var
 	void *win;
 	int height;
 	int width;
-	int serp_head[2];
-	char serp[5];
-	int point[3][2];
+	int snack_head[2];
+	char *snack;
+	int point[points_len][2];
 } t_var;
 
 int draw_rect(t_var *var, int x_start, int y_start, int height, int width, int color)
@@ -43,32 +44,51 @@ int draw_serpent(t_var *var)
 {
 	draw_rect(var, 0, 0, var->height, var->width, 0); // draw black rect
 	int j = 0;
-	while (j < 3)
+	while (j < points_len)
 	{
 		draw_rect(var, var->point[j][0], var->point[j][1], len, len, green); // draw point
 		j++;
 	}
 
 	// draw_rect(var, 0, 0, len, len, blue);
-	int i = strlen(var->serp);
+	int i = strlen(var->snack);
 	int x;
 	int y;
-	x = var->serp_head[0] - len;
-	y = var->serp_head[1];
+	x = var->snack_head[0] - len;
+	y = var->snack_head[1];
 	while (i > 0)
 	{
-		if (var->serp[i] == 'r')
+		if (var->snack[i] == 'r')
 			x -= len;
-		if (var->serp[i] == 'l')
+		if (var->snack[i] == 'l')
 			x += len;
-		if (var->serp[i] == 'u')
+		if (var->snack[i] == 'u')
 			y += len;
-		if (var->serp[i] == 'd')
+		if (var->snack[i] == 'd')
 			y -= len;
 		draw_rect(var, x, y, len, len, red);
 		i--;
 	}
 	return (0);
+}
+
+char *increase_snack(char *snack)
+{
+	char to_add = snack[0];
+	char *new_snack = malloc(strlen(snack) + 1);
+
+	new_snack[0] = to_add;
+	int i = 0;
+	while (i < strlen(snack))
+	{
+		new_snack[i + 1] = snack[i];
+		printf("%d -> %c\n", i, snack[i]);
+		i++;
+	}
+	new_snack[i + 1] = '\0';
+	printf("========================\n");
+	free(snack);
+	return new_snack;
 }
 
 int clicked_key(int keycode, t_var *var)
@@ -81,78 +101,84 @@ int clicked_key(int keycode, t_var *var)
 	if (keycode == 126)
 	{
 		//	write(1,"Up\n",4);
-		var->serp_head[1] -= len;
+		var->snack_head[1] -= len;
 		int i = 1;
-		while (i < strlen(var->serp) - 1)
+		while (i < strlen(var->snack) - 1)
 		{
-			var->serp[i] = var->serp[i + 1];
+			var->snack[i] = var->snack[i + 1];
 			i++;
 		}
-		var->serp[i] = 'u';
+		var->snack[i] = 'u';
 	}
 	if (keycode == 125)
 	{
 		// write(1,"Down\n",6);
-		var->serp_head[1] += len;
+		var->snack_head[1] += len;
 		int i = 1;
-		while (i < strlen(var->serp) - 1)
+		while (i < strlen(var->snack) - 1)
 		{
-			var->serp[i] = var->serp[i + 1];
+			var->snack[i] = var->snack[i + 1];
 			i++;
 		}
-		var->serp[i] = 'd';
+		var->snack[i] = 'd';
 	}
 	if (keycode == 123)
 	{
 		// write(1,"Left\n",6);
-		var->serp_head[0] -= len;
+		var->snack_head[0] -= len;
 		int i = 1;
-		while (i < strlen(var->serp) - 1)
+		while (i < strlen(var->snack) - 1)
 		{
-			var->serp[i] = var->serp[i + 1];
+			var->snack[i] = var->snack[i + 1];
 			i++;
 		}
-		var->serp[i] = 'l';
+		var->snack[i] = 'l';
 	}
 	if (keycode == 124)
 	{
 		//	write(1,"Right\n",7);
-		var->serp_head[0] += len;
+		var->snack_head[0] += len;
 		int i = 1;
-		while (i < strlen(var->serp) - 1)
+		while (i < strlen(var->snack) - 1)
 		{
-			var->serp[i] = var->serp[i + 1];
+			var->snack[i] = var->snack[i + 1];
 			i++;
 		}
-		var->serp[i] = 'r';
+		var->snack[i] = 'r';
 	}
-	//	printf("(%d, %d) -> (%d, %d)\n", var->serp_head[0], var->serp_head[1], var->point[0], var->point[1]);
-	draw_serpent(var);
+	//	printf("(%d, %d) -> (%d, %d)\n", var->snack_head[0], var->snack_head[1], var->point[0], var->point[1]);
+
 	i = 0;
-	while (i < 3)
+	while (i < points_len)
 	{
-		if (var->serp_head[0] - len == var->point[i][0] && var->serp_head[1] == var->point[i][1])
+		if (var->snack_head[0] - len == var->point[i][0] && var->snack_head[1] == var->point[i][1])
 		{
 			printf("find point %d from ", i);
-			if(keycode == 126)
+			if (keycode == 126)
 			{
 				printf("126 down\n");
 			}
-			if(keycode == 125)
+			if (keycode == 125)
 			{
 				printf("125 top\n");
 			}
-			if(keycode == 124)
+			if (keycode == 124)
 			{
 				printf("124 left\n");
 			}
-			if(keycode == 123)
+			if (keycode == 123)
 			{
 				printf("123 right\n");
 			}
+			printf("old len %lu\n", strlen(var->snack));
+			var->snack = increase_snack(var->snack);
+			printf("new len %lu\n", strlen(var->snack));
+			var->point[i][0] = -100;
+			var->point[i][1] = -100;
 		}
 		i++;
 	}
+	draw_serpent(var);
 	// draw_rect(var, 400, 250, len, len, red);
 	return (0);
 }
@@ -177,7 +203,7 @@ int main(void)
 
 	int i = 0;
 
-	while (i < 3)
+	while (i < points_len)
 	{
 		var.point[i][0] = the_num(rand() % var.width);
 		var.point[i][1] = the_num(rand() % var.height);
@@ -207,14 +233,15 @@ int main(void)
 		i++;
 	}
 
-	var.serp[0] = 'r';
-	var.serp[1] = 'r';
-	var.serp[2] = 'r';
-	var.serp[3] = 'd';
-	var.serp[4] = 'r';
+	var.snack = malloc(5);
+	var.snack[0] = 'r';
+	var.snack[1] = 'r';
+	var.snack[2] = 'r';
+	var.snack[3] = 'd';
+	var.snack[4] = 'r';
 
-	var.serp_head[0] = 80;
-	var.serp_head[1] = 40;
+	var.snack_head[0] = 80;
+	var.snack_head[1] = 40;
 
 	draw_serpent(&var);
 	mlx_key_hook(var.win, clicked_key, &var);
